b49e94e888f940eb7d73b556974167864a20c9f2
s-kyono <s-kyono@b-tm.co.jp>
2021-03-01 18:40:57 +0900

    index.jsの修正

--- web/src/index.js
+++ web/src/index.js
@@ -7,18 +7,44 @@ import TextBox from "./components/TextBox";
 import Component from "./components/Component";
 import Anchor from "./components/Anchor";
 import Button from "./components/Button";
+import FormRequestName from "./components/FormRequestName";
+import EssayForm from "./components/EssayForm";
+import FlavorForm from "./components/FlavorForm";
+import Reservation from "./components/Reservation";
+import Calculator from "./components/LiftUp/Calculator";
+
 function App() {
+  const test =
+    {
+      grapefruit : {
+        index : 'grapefruit', name : 'Grapefruit'
+      },
+      lime : {
+        inex : 'lime', name : 'Lime'
+      },
+      coconut : {
+        inex : 'coconut', name : 'Coconut'
+      },
+      mango : {
+        inex : 'mango', name : 'Mango'
+      },
+    };
   return (
     <div className="container">
+      <Calculator />
       <Hello name="ozaki" />
       <Anchor />
       <Button />
-      <YourName src="img/test.jpg"/>
+      <YourName src="img/test.jpg" />
       <TimeTick />
       <TextBox />
       <Component name="test" text="value" url="img/dog.png" date="2021-03-01" />
       <TimeTick />
       <TimeTick />
+      <FormRequestName />
+      <EssayForm />
+      <FlavorForm fruits = { test }/>
+      <Reservation />
     </div>
   );
 }
